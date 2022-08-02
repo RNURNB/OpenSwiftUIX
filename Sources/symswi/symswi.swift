c7f8d45716367c660ad38d05c6fae794eace9462
Rene Nürnberger <rene.nuernberger@web.de>
2022-08-02 16:41:14 +0200

    xxx

---
+++ Sources/symswi/symswi.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+public func testsymswi() {
+    print("test symswi")
+}
---
+++ package.swift
@@ -0,0 +1,18 @@
+import PackageDescription
+
+
+let platformTargets: [Target] = [
+    .target(name: "symswi"),
+    
+]
+let platformProducts: [Product] =  [
+  .library(name: "symswi", targets: ["symswi"]),
+]
+
+
+let package = Package(
+    name: "symswi",
+    products: platformProducts,
+    targets: platformTargets,
+    cxxLanguageStandard: .cxx11
+)
